92369834244aaeec781304518581f4f8eec13894
patrickrugabano1-code <PRugabano@csi.edu>
2026-08-21 12:52:38 -0600

    Create variables-homework.py

---
+++ variables-homework.py
@@ -0,0 +1,16 @@
+#a variable= a reusable container for storing a value
+#a variable behaves as if it were the value it contains  example
+age = 25
+gpa = 3.2
+distance = 2.5
+name = "Patrick"
+
+print("you are"+ str(age) + "years old")
+print("you are",age,"years old")
+
+#f-string are becoming more popilar for output
+print(f"you are {age} years old")
+
+# concatenation example - joining with +
+print("Your gpa is " + str(gpa))
+print("Hello " + name + ", you ran " + str(distance) + "km")
