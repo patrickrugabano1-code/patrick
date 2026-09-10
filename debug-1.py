1b076c7594955c31bc86086e3ce0d72aefd2aeb4
patrickrugabano1-code <PRugabano@csi.edu>
2026-09-10 16:14:28 -0600

    Add files via upload

--- debug-1.py
+++ debug-1.py
@@ -1,13 +1,26 @@
-# Intro to Programming
-# Debug Exercise 2
-
-# This program should add three numbers together.
+# This program adds three numbers
 
 total = 0
-num1 = input("What's the first number? >")
-total = num1
-num2 = input("What's the second number? >")
-total = num2
-num3 = input("What's the third number? >")
-total = num3
-print(f"Total is: {total}")
+
+num1 = int(input("First number? >"))
+total = total + num1
+
+num2 = int(input("Second number? >"))
+total = total + num2
+
+num3 = int(input("Third number? >"))
+total = total + num3
+
+print("Total is:", total)
+
+#project one
+
+first = input("First name? ")
+last = input("Last name? ")
+print(len(first), len(last))
+print(first[0], last[0])
+
+#project one part 2
+
+dollars = float(input("Dollars? "))
+print(dollars * 1350)
